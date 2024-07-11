d6b9a2a84ff6f70591dce256ee562c2cae4ce64f
dobisfreee <amber981001@gmail.com>
2024-07-11 19:47:48 +0900

    (2024.7.11) 브루트 포스

---
+++ Program/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool IsPrime(int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+	
+}
+
+int main()
+{
+	
+
+	return 0;
+}
