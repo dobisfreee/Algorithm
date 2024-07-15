c2725ed62dd32faba662771ec4d5b4b71f941a4c
dobisfreee <amber981001@gmail.com>
2024-07-15 19:46:14 +0900

    (2024.7.15) 삽입 정렬

---
+++ Class4th(Selection Sort/Source.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+void SelectionSort(int a, int b, int c, int d, int e)
+{
+	int list[] = { a,b,c,d,e };
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		int min = list[i];
+		int select = i;
+
+		for (int j = i + 1; j < SIZE; j++)
+		{
+			if (min > list[j])
+			{
+				min = list[j];
+				select = j;
+			}
+		}
+
+		swap(list[i], list[select]);
+	}
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+
+}
+
+int main()
+{
+#pragma region 선택 정렬
+	// 주어진 리스트 중에 최소값을 찾아서 맨 앞에 위치한 결과를 
+	// 교체하는 방식으로 정렬하는 알고리즘입니다. 
+
+	SelectionSort(10, 3, 5, 2, 1);
+
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,55 +1,8 @@
 ﻿#include <iostream>
 
-#define SIZE 5
-
 using namespace std;
-
-void BubbleSort()
-{
-	int list[SIZE];
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		int insert = 0;
-
-		cin >> insert;
-
-		list[i] = insert;
-	}
-
-	int temp = 0;
-
-	for (int i = 0; i < SIZE - 1; i++)
-	{
-		for (int j = 0; j < SIZE - (i+1); j++)
-		{
-			if (list[j] > list[j + 1])
-			{
-				temp = list[j + 1];
-				list[j + 1] = list[j];
-				list[j] = temp;
-			}
-		}
-	}
-
-	for (const int & element : list)
-	{
-		cout << element << " ";
-	}
-
-}
 
 int main()
 {
-#pragma region 거품 정렬
-	// 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘입니다. 
-
-	BubbleSort();
-
-	
-
-#pragma endregion
-
-
 	return 0;
 }
