97e8ed4e2ddea1673d6acc19ea86e2be86186232
dobisfreee <amber981001@gmail.com>
2024-07-17 19:46:51 +0900

    (2024.7.17) 분할 정복

--- Program/Source.cpp
+++ Program/Source.cpp
@@ -2,42 +2,24 @@
 
 using namespace std;
 
-#define SIZE 10
+#define SIZE 7
 
-void BinarySearch(int list[], int key)
+int QuickSort(int list[])
 {
-	int left = 0;
-	int right = SIZE - 1;
-	
-	while (left <= right)
-	{
-		int middle = (left + right) / 2;
 
-		if (list[middle] == key)
-		{
-			cout << "Key Found in the " << middle << "th Position" << endl;
-			return;
-		}
-		else if (list[middle] < key)
-		{
-			left = middle + 1;
-		}
-		else
-		{
-			right = middle - 1;
-		}
-	}
-	cout << "Not Found" << endl;
 }
+
 int main()
 {
-#pragma region 이진 탐색
-	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를 
-	// 좁혀나가는 방식으로 동작하는 탐색 알고리즘입니다. 
+#pragma region 퀵 정렬
+	// 기준점(pivot)을 획득한 다음 해당 기준점을 기준으로 배열을 나누고
+	// 한 쪽에는 기준점보다 작은 값들이 위치하고 다른 한 쪽에는 기준점보다
+	// 큰 값들이 위치하도록 정렬하는 알고리즘입니다. 
 
-	int list[SIZE] = { 5,6,11,13,27,55,66,99 };
+	// 나누어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
+	// 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬합니다. 
 
-	BinarySearch(list, 55);
+	int list[SIZE] = 
 #pragma endregion
 
 	return 0;
