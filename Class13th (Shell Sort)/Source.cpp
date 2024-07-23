550612350ed44fdc3f8e5691f2e3559074985b48
dobisfreee <amber981001@gmail.com>
2024-07-23 19:47:13 +0900

    (2024.7.23) 쉘 정렬

---
+++ Class13th (Shell Sort)/Source.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 11
+
+int main()
+{
+#pragma region 쉘 정렬(3중 for문)
+	// 먼 거리의 요소들을 먼저 정렬하여 배열을 부분적으로
+	// 정렬한 후, 점진적으로 더 작은 간격을 사용하는 정렬 알고리즘입니다. 
+
+	// int gap = 배열의 크기 / 2 , 짝수면 + 1 gap은 인덱스 사이의 차이 
+
+	// int key = gap의 인덱스 
+
+	// key 와 0번째 인덱스 비교, key값이 더 작으면 앞에 삽입
+
+	// 0 += 1 , gap =+1 번째 인덱스 비교 
+
+	// gap만큼 반복 
+
+	// 1회차 끝났으면 gap = gap / 2 를 해준다 
+
+	int list[SIZE] = { 10,8,6,20,4,3,22,1,0,15,16 };
+
+	int gap = 0;
+
+	int j = 0;
+	int key = 0;
+
+	for (gap = SIZE / 2; gap > 0; gap /= 2)
+	{
+		if (gap % 2 == 0)
+		{
+			gap++;
+		}
+
+		for (int i = gap; i < SIZE; i++)
+		{
+			key = list[i];
+
+			for (j = i; j >= gap && list[j - gap] > key; j -= gap)
+			{
+				list[j] = list[j - gap];
+			}
+
+			list[j] = key;
+		}
+	}
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -2,47 +2,29 @@
 
 using namespace std;
 
-#define SIZE 7
+void TheGreatestCommonFactor(int a,int b)
+{
+	int answer= 1;
+
+	for (int i = 2; i <= a && i <= b; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+		{
+			answer *= i;
+
+			a /= i;
+			b /= i;
+
+			i--;
+		}
+	}
+
+	cout << "최대 공약수 : " << answer;
+}
+
 int main()
 {
-#pragma region 쉘 정렬(3중 for문)
-	// 먼 거리의 요소들을 먼저 정렬하여 배열을 부분적으로
-	// 정렬한 후, 점진적으로 더 작은 간격을 사용하는 정렬 알고리즘입니다. 
-
-	// int gap = 배열의 크기 / 2 , 짝수면 + 1 gap은 인덱스 사이의 차이 
-
-	// int key = gap의 인덱스 
-
-	// key 와 0번째 인덱스 비교, key값이 더 작으면 앞에 삽입
-
-	// 0 += 1 , gap =+1 번째 인덱스 비교 
-
-	// gap만큼 반복 
-
-	// 1회차 끝났으면 gap = gap / 2 를 해준다 
-
-	int list[SIZE] = { 10,4,3,20,8,6,22 };
-
-	int gap = 0; 
-
-	if (gap % 2 != 0)
-	{
-		gap = SIZE / 2; // 3
-	}
-	else
-	{
-		gap = (SIZE / 2) + 1;
-	}
-
-	int key = list[gap]; // 20
-
-	for (int i = 0; i < gap; i++)
-	{
-
-	}
-
-
-#pragma endregion
+	TheGreatestCommonFactor(15, 30);
 
 	return 0;
 }
