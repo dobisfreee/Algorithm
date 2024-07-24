7a4a0ed6adba109622fb4781af6ddb71a3012c93
dobisfreee <amber981001@gmail.com>
2024-07-24 19:46:33 +0900

    (2024.7.24) 백트래킹

---
+++ Class14th (Euclid)/Source.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+#include <math.h>
+
+using namespace std;
+
+void TheGreatestCommonFactor(int a, int b)
+{
+	int answer = 0;
+
+	for (int i = 1; i <= min(a, b); i++)
+	{
+		if (a % i == 0 && b % i == 0)
+		{
+			answer = i;
+		}
+	}
+
+	cout << "최대 공약수 : " << answer;
+}
+
+int Euclid(int x, int y)
+{
+	int a = max(x, y);
+	int b = min(x, y);
+
+	if (a % b == 0) // 재귀 루프 탈출하는 조건
+	{
+		return b;
+	}
+	else
+	{
+		Euclid(b, a % b);
+	}
+}
+
+int Euclid1(int x, int y)
+{
+	if (y == 0)
+	{
+		return x;
+	}
+	else
+	{
+		Euclid(y, x % y);
+	}
+}
+
+int main()
+{
+#pragma region 유클리드 호제법
+	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로, 
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다. 
+#pragma endregion
+
+	// TheGreatestCommonFactor(15, 30);
+
+	int x = 24;
+	int y = 18;
+
+	cout << "최대 공약수는 : " << Euclid(x, y);
+
+	return 0;
+}
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -2,29 +2,37 @@
 
 using namespace std;
 
-void TheGreatestCommonFactor(int a,int b)
+void Hanoi(int n, char from, char by, char to) // 3, A, B, C
 {
-	int answer= 1;
-
-	for (int i = 2; i <= a && i <= b; i++)
+	if (n == 1)
 	{
-		if (a % i == 0 && b % i == 0)
-		{
-			answer *= i;
-
-			a /= i;
-			b /= i;
-
-			i--;
-		}
+		cout << from << "->" << to << endl;
 	}
+	else
+	{
+		Hanoi(n - 1, from, to, by);
+		cout << from << "->" << to << endl;
+		Hanoi(n - 1, by, from, to);
 
-	cout << "최대 공약수 : " << answer;
+	}
 }
-
 int main()
 {
-	TheGreatestCommonFactor(15, 30);
+#pragma region 백트래킹
+	// 해를 찾아가는 도중에 지금의 경로가 해가 될 것 같지 않으면
+	// 더 이상 깊이 들어가지 않고 이전 단계로 다시 돌아가는 알고리즘입니다. 
+
+	// 백트래킹의 유망성 판단
+	// 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면
+	// 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동합니다.
+
+	// 해가 될 만한 가능성이 있으면 유망하다. (Promising)
+	// 유망하지 않은 노드에 가지 않는 것.	  (Pruning)
+
+	Hanoi(3, 'A', 'B', 'C');
+#pragma endregion
+
+
 
 	return 0;
 }
