7d3f8efa298f61bd32e6bd4e1a601818dd361eb0
dobisfreee <amber981001@gmail.com>
2024-07-31 19:46:11 +0900

    (2024.7.31) 누적합

---
+++ Class20th(Minimum Spinning Tree)/Source.cpp
@@ -0,0 +1,159 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define SIZE 8
+
+class Graph
+{
+private:
+
+	int cost;
+	int parent[SIZE];
+
+	class Edge
+	{
+	private:
+		int x;
+		int y;
+		int distance;
+
+
+
+
+	public:
+
+		Edge(int x, int y, int distance)
+		{
+			this->x = x;
+			this->y = y;
+			this->distance = distance;
+		}
+
+		int& X()
+		{
+			return x;
+		}
+
+		int& Y()
+		{
+			return y;
+		}
+
+		int& Distance()
+		{
+			return distance;
+		}
+
+		bool operator < (const Edge& edge) // 연산자 오버로딩 복습하기, 값 맘대로 수정못하게 const
+		{
+			return this->distance < edge.distance;
+		}
+	};
+
+	vector <Edge> graph;
+
+public:
+
+	Graph()
+	{
+		cost = 0;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			parent[i] = i;
+		}
+	}
+
+	void Insert(int x, int y, int distance)
+	{
+		graph.push_back(Edge(x, y, distance));
+	}
+
+	void Kruskal()
+	{
+		sort(graph.begin(), graph.end());
+
+		for (int i = 0; i < graph.size(); i++)
+		{
+			if (Same(graph[i].X(), graph[i].Y()) == false)
+			{
+				cost += graph[i].Distance();
+				Union(graph[i].X(), graph[i].Y());
+			}
+		}
+
+		cout << "Graph 최소 비용 : " << cost << endl;
+	}
+
+	int Find(int x)
+	{
+		if (x == parent[x])
+		{
+			return x;
+		}
+		else
+		{
+			return parent[x] = Find(parent[x]);
+		}
+	}
+
+	void Union(int x, int y)
+	{
+		x = Find(x);
+		y = Find(y);
+
+		if (x < y)
+		{
+			parent[y] = x;
+		}
+		else
+		{
+			parent[x] = y;
+		}
+	}
+
+	bool Same(int x, int y)
+	{
+		return  Find(x) == Find(y);
+	}
+};
+
+int main()
+{
+#pragma region 신장 트리
+	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
+	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다. 
+
+	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개입니다. 
+
+	// 최소 비용 신장 트리
+	// 그래프의 간선들의 가중치 합이 최소인 신장 트리 
+
+	// 거리가 작은 순으로 vertex를 서로 연결
+
+	Graph graph;
+
+	graph.Insert(1, 7, 10);
+	graph.Insert(1, 4, 28);
+	graph.Insert(1, 2, 66);
+	graph.Insert(1, 5, 19);
+
+	graph.Insert(2, 4, 25);
+	graph.Insert(2, 5, 60);
+
+	graph.Insert(3, 5, 21);
+	graph.Insert(3, 6, 35);
+
+	graph.Insert(4, 7, 15);
+	graph.Insert(5, 6, 40);
+
+	graph.Kruskal();
+
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,109 +1,36 @@
 ﻿#include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-#define SIZE 8
-class Graph
-{
-private:
-	class Edge
-	{
-	private:
-		int x;
-		int y;
-		int distance;
-
-		int parent[SIZE];
-
-	public:
-
-		Edge(int x, int y, int distance) 
-		{
-			this->x = x;
-			this->y = y;
-			this->distance = distance;
-
-			for (int i = 1; i < SIZE; i++)
-			{
-				parent[i] = i;
-			}
-		}
-
-		int& X()
-		{
-			return x;
-		}
-
-		int& Y()
-		{
-			return y;
-		}
-
-		int& Distance()
-		{
-			return distance;
-		}
-	};
-
-	vector <Edge> graph;
-
-public:
-	Graph()
-	{
-		
-	}
-
-	void Insert(int x, int y, int distance)
-	{
-		graph.push_back(Edge(x,y,distance));
-	}
-
-	void Kruskal() // cf) end는 순방향, c(onverse)end는 역방향 
-	{
-		sort(graph.begin(), graph.end());
-
-		int sum = 0;
-
-		for (int i = 0; i < graph.size(); i++)
-		{
-			sum += graph[i].Distance();
-		}
-	}
-};
+#define SIZE 5
 
 int main()
 {
-#pragma region 신장 트리
-	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
-	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다. 
+#pragma region 누적 합
+	// 특정한 배열이 있을 때, 배열까지의 합을 나타내는 것입니다. 
 
-	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개입니다. 
+	int list[SIZE] = { 1,2,3,4,5 };
+	int sumList[SIZE+1];
 
-	// 최소 비용 신장 트리
-	// 그래프의 간선들의 가중치 합이 최소인 신장 트리 
+	for (int i = 0; i < SIZE+1; i++)
+	{
+		if (i == 0)
+		{
+			sumList[i] = 0;
+		}
+		else
+		{
+			sumList[i] = sumList[i - 1] + list[i-1];
+		}
+	}
 
-	// 거리가 작은 순으로 vertex를 서로 연결
+	for (const int& element : sumList)
+	{
+		cout << element << " ";
+	}
 
-	Graph graph;
-
-	graph.Insert(1, 7, 10);
-	graph.Insert(1, 4, 28);
-	graph.Insert(1, 2, 66);
-	graph.Insert(1, 5, 19);
-
-	graph.Insert(2, 4, 25);
-	graph.Insert(2, 5, 60);
-
-	graph.Insert(3, 5, 21);
-	graph.Insert(3, 6, 35);
-
-	graph.Insert(4, 7, 15);
-	graph.Insert(5, 6, 40);
-
+	
 #pragma endregion
-
 
 	return 0;
 }
